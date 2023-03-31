1e94bf4ad19484515816062e4cb0730a00d21d6a
iklaq <iklaq.ahmad@jalantechnologies.com>
2023-03-31 15:28:06 +0530

    fixed

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,10 +1,51 @@
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+
 const LoginPage = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const login = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:4000/login", {
+        method: "POST",
+        body: JSON.stringify({ userName, password }),
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+
+      if (response.ok) {
+        alert("login successful");
+        setRedirect(true);
+      } else {
+        alert("login failed");
+      }
+    } catch (e) {
+      console.log("try again");
+    }
+  };
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
     <div className="login-page">
       <h1>LOGIN</h1>
-      <form className="form" action="">
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
+      <form className="form" onSubmit={login}>
+        <input
+          type="text"
+          placeholder="username"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button>Login</button>
       </form>
     </div>
