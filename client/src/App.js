074d725f30a879718470606a29916d5ca4ea0b6a
iklaq <iklaq.ahmad@jalantechnologies.com>
2023-04-03 01:04:18 +0530

    added create post frontend

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import LoginPage from "./components/LoginPage";
 import Post from "./components/Post";
 import RegisterPage from "./components/RegisterPage";
 import { UserContextProvider } from "./UserContext";
+import CreatePost from "./components/CreatePost";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route index element={<Post />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/create" element={<CreatePost/>}/>
       </Route>
     </Routes>
     </UserContextProvider>
---
+++ client/src/components/CreatePost.js
@@ -0,0 +1,15 @@
+const CreatePost = () => {
+
+
+  return (
+
+   <form>
+    <input type="text" name="title" placeholder="Title"/>
+    <input type="file" />
+    <textarea name="" id="" cols="30" rows="10"></textarea>
+   </form>
+  
+   )
+}
+
+export default CreatePost;
